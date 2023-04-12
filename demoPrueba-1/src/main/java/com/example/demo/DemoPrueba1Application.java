b43e73bc398f20a00bc77cc9c7e9758f58d6b7ed
camilagarcia36 <garciiaperezz@gmail.com>
2023-04-12 12:13:28 +0200

    maven

--- demoPrueba-1/src/main/java/com/example/demo/DemoPrueba1Application.java
+++ demoPrueba-1/src/main/java/com/example/demo/DemoPrueba1Application.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,14 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class DemoPrueba1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoPrueba1Application.class, args);
 	}
 	
 	 @GetMapping("/hello")
 	    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 	      return String.format("Hello %s!", name);
 	    }
+
 }
